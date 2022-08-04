6e5ea1439b7be2e49c28a73e2194a13e7ab00c6b
Leonardo José Nunes <leonardo.ferax@gmail.com>
2022-08-04 14:50:54 -0300

    remove folder and group function

---
+++ src/actions/remove/remove_folder.sh
@@ -0,0 +1,44 @@
+
+remove_folder() {
+  check_user_permission
+
+  # Show list of folders and ask one to be removed
+  echo "Choose an option from the iac_fuga groups: "
+  echo
+  get_field 1 "$IAC_FOLDERS" | sed 1d 
+  echo
+  echo -n "Which folder do you want to remove?: "
+  read folderName
+  echo
+
+  check_empty "$folderName"
+
+  $( has_key "$folderName" "$IAC_FOLDERS" ) && {
+    delete_registry "$folderName" "$IAC_FOLDERS"
+    echo
+    echo "Folder '$folderName' has been removed from iac_fuga system."
+  }
+
+  $(! check_system_folder ${IAC_PATH}${folderName}) && {
+    echo "Folder ${IAC_PATH}${folderName} is not in OS"
+  }
+
+  $( check_system_folder "${IAC_PATH}${folderName}" ) && {
+    folderGroup=$(uppercase "GRP_${folderName}")
+    echo
+
+    $( check_system_folder "${IAC_PATH}${folderName}") && {
+      rm -r -I "${IAC_PATH}${folderName}" 
+      echo
+      echo "Folder $folderName has been removed from '"$OSTYPE"'."
+      echo
+    }
+
+    $( check_system_group "$folderGroup" ) && {
+      groupdel "${folderGroup}"
+      echo
+      echo "Group '${folderGroup}' has been removed from '"$OSTYPE"'."
+      echo
+    }
+  }
+}
