eaac46847b5c5f1c17c839e1327fce3b1446651e
Leonardo José Nunes <leonardo.ferax@gmail.com>
2022-08-04 18:17:17 -0300

    remove user function

---
+++ src/actions/remove/remove_user.sh
@@ -0,0 +1,26 @@
+remove_user() {
+  check_user_permission
+  # Show list of folders and ask one to be removed
+  echo "iac_fuga users list:"
+  echo
+  get_field 1 "$IAC_USERS" | sed 1d 
+  echo
+  echo -n "Which USER do you want to remove?: "
+  read userName
+
+  grep "^$userName$SEP" "/etc/passwd" && {
+    userdel -rf "$userName"
+    echo
+    echo "User $userName has been removed"
+    echo 
+  }
+
+  grep "^$userName$SEP" "$IAC_USERS" && {
+    $( delete_registry  "$userName" "$IAC_USERS" )
+    echo
+    echo "User $userName has been removed from IAC_USERS"
+    echo 
+  }
+  
+  exit 0
+}
--- src/lib/global_variables.sh
+++ src/lib/global_variables.sh
@@ -1,11 +1,11 @@
 # Global variables
-IAC_PATH="/"                            # Path to create folders
-IAC_FOLDERS="./src/data/folders.txt"    # File containing folders information
-IAC_USERS="./src/data/users.txt"        # File containing users information
-IAC_SHELL="/bin/bash"                  # Shell to be used
+IAC_PATH="/"                              # Path to create folders
+IAC_FOLDERS="./src/data/folders.txt"      # File containing folders information
+IAC_USERS="./src/data/users.txt"          # File containing users information
+IAC_SHELL="/bin/bash"                     # Shell to be used
 IAC_DEFAULT_PASS="Senha123"
 DUMMY_FOLDERS="./src/assets/dummy_folders.txt"
 DUMMY_USERS="./src/assets/dummy_users.txt"
 
-SEP=:                                   # Default delimiter
-TEMP="/tmp/temp.$$"                     # Temp file 
+SEP=:                                     # Default delimiter
+TEMP="/tmp/temp.$$"                       # Temp file 
